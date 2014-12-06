3ae6dbdabcba9b57dc8a4ad13741bddce506f836
fschwiet <fschwiet@gmail.com>
2014-12-06 14:34:19 -0600

    using Selenium to scrape some verb info

--- card-scrape/GetVerbCommand.cs
+++ card-scrape/GetVerbCommand.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using ManyConsole;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 
 namespace cardscrape
 {
@@ -22,6 +26,42 @@ namespace cardscrape
 
 		public override int Run (string[] remainingArguments)
 		{
+			var options = new ChromeOptions();
+			var service = ChromeDriverService.CreateDefaultService();
+			service.SuppressInitialDiagnosticInformation = true;
+
+			using(var driver = new ChromeDriver(service, options)) {
+
+				driver.Navigate ().GoToUrl ("http://www.spanishdict.com/translate/" + Verb);
+
+				var translationDiv = driver.FindElementsByCssSelector (".card .quickdef .lang").FirstOrDefault();
+				var conjugateLink = driver.FindElementsByCssSelector (".card a[href^='http://www.spanishdict.com/conjugate']").FirstOrDefault();
+
+				if (translationDiv == null) {
+					Console.WriteLine ("Unable to find translation of '" + Verb + "'.");
+					return 1;
+				}
+
+				if (conjugateLink == null) {
+					Console.WriteLine ("Is '" + Verb + "' a verb?  Unable to find conjugation link.");
+					return 1;
+				}
+
+				conjugateLink.Click ();
+
+				var term = driver.FindElementByCssSelector (".card .quickdef .source-text").Text.Trim ();
+				var translation = driver.FindElementByCssSelector (".card .quickdef .lang").Text.Trim();
+
+				if (term.ToLowerInvariant () != Verb.ToLower ()) {
+					Console.WriteLine ("The verb provided '" + Verb + "' does not match the base form found '" + term + "'.  Please use the base form.");
+					return 1;
+				}
+
+				Console.WriteLine (term + " - " + translation);
+
+				System.Threading.Thread.Sleep (5000);
+			}
+
 			return 0;
 		}
 	}
