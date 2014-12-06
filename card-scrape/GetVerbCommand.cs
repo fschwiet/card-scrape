8a37f63abcb17e115b0ce4f2c999a239cf5173a5
fschwiet <fschwiet@gmail.com>
2014-12-06 13:49:01 -0600

    include a basic command using ManyConsole

---
+++ card-scrape/GetVerbCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ManyConsole;
+
+namespace cardscrape
+{
+	public class GetVerbCommand : ConsoleCommand
+	{
+		public string Verb;
+
+		public GetVerbCommand ()
+		{
+			this.IsCommand ("get-verb", "Generates notes for a Spanish verb");
+			this.HasAdditionalArguments (1, " <verb>");
+		}
+
+		public override int? OverrideAfterHandlingArgumentsBeforeRun (string[] remainingArguments)
+		{
+			Verb = remainingArguments [0];
+
+			return base.OverrideAfterHandlingArgumentsBeforeRun (remainingArguments);
+		}
+
+		public override int Run (string[] remainingArguments)
+		{
+			return 0;
+		}
+	}
+}
+
