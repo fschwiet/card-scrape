cab754ba7db9165138f75c04090eb5d9a6b46fa3
fschwiet <fschwiet@gmail.com>
2014-12-11 13:51:08 -0600

    whitespace, more {}s, and moving change of timeout

--- card-scrape/GetVerbCommand.cs
+++ card-scrape/GetVerbCommand.cs
@@ -62,127 +62,127 @@ namespace cardscrape
 			var service = ChromeDriverService.CreateDefaultService();
 			service.SuppressInitialDiagnosticInformation = true;
 
-			using (var driver = new ChromeDriver(service, options))
-			foreach (var inputVerb in Verbs) {
+			using (var driver = new ChromeDriver (service, options)) {
 
 				driver.Manage ().Timeouts ().ImplicitlyWait (TimeSpan.FromSeconds (5));
 
-				var verb = inputVerb.Verb;
-			
-				driver.Navigate ().GoToUrl ("http://www.spanishdict.com/translate/" + verb);
-
-				var translationDiv = driver.FindElementsByCssSelector (".card .quickdef .lang").FirstOrDefault();
-				var conjugateLink = driver.FindElementsByCssSelector (".card a[href^='http://www.spanishdict.com/conjugate']").FirstOrDefault();
-
-				if (translationDiv == null) {
-					Console.WriteLine ("Unable to find translation of '" + verb + "'.");
-					return 1;
-				}
-
-				if (conjugateLink == null) {
-					Console.WriteLine ("Is '" + verb + "' a verb?  Unable to find conjugation link.");
-					return 1;
-				}
-
-				// for some undetermined reason conjugateLink is reported as not visible for verb 'ir'
-				// so we can't just conjugateLink.Click (), instead we use javascript
-				driver.ExecuteScript ("arguments[0].click();", conjugateLink);
-
-				var term = driver.FindElementByCssSelector (".card .quickdef .source-text").Text.Trim ();
-				var translation = driver.FindElementByCssSelector (".card .quickdef .lang").Text.Trim();
-
-				if (inputVerb.ExtraInfo != null) {
-					translation = translation + " " + inputVerb.ExtraInfo;
-				}
-
-				if (term.ToLowerInvariant () != verb.ToLower ()) {
-					Console.WriteLine ("The verb provided '" + verb + "' does not match the base form found '" + term + "'.  Please use the base form.");
-					return 1;
-				}
-
-				foreach (var vtableLabel in driver.FindElementsByCssSelector ("a.vtable-label")) {
-					var vtableType = vtableLabel.Text.Trim ().ToLower().Replace(" ", "-");
-					driver.ExecuteScript("arguments[0].classList.add('vtable-label-" + vtableType + "');",vtableLabel);
-				}
+				foreach (var inputVerb in Verbs) {
 				
-				var infinitiveResult = new Result () {
-					Term = term,
-					TermDefinition = translation
-				};
+					driver.Navigate ().GoToUrl ("http://www.spanishdict.com/translate/" + inputVerb.Verb);
 
-				List<Result> results = new List<Result> ();
+					var translationDiv = driver.FindElementsByCssSelector (".card .quickdef .lang").FirstOrDefault ();
+					var conjugateLink = driver.FindElementsByCssSelector (".card a[href^='http://www.spanishdict.com/conjugate']").FirstOrDefault ();
 
-				results.Add(infinitiveResult);
+					if (translationDiv == null) {
+						Console.WriteLine ("Unable to find translation of '" + inputVerb.Verb + "'.");
+						return 1;
+					}
 
-				var indicativeTable = driver.FindElementByCssSelector (".vtable-label-indicative + .vtable-wrapper");
+					if (conjugateLink == null) {
+						Console.WriteLine ("Is '" + inputVerb.Verb + "' a verb?  Unable to find conjugation link.");
+						return 1;
+					}
 
-				var columnNames = indicativeTable.FindElements (By.CssSelector ("tr:first-child td")).Select (e => e.Text.ToLower()).Skip (1).ToArray ();
-				var rowNames = indicativeTable.FindElements (By.CssSelector ("tr td:first-child")).Select (e => e.Text.ToLower()).Skip (1).ToArray ();
+					// for some undetermined reason conjugateLink is reported as not visible for verb 'ir'
+					// so we can't just conjugateLink.Click (), instead we use javascript
+					driver.ExecuteScript ("arguments[0].click();", conjugateLink);
 
-				for (var column = 0; column < columnNames.Length; column++) {
+					var term = driver.FindElementByCssSelector (".card .quickdef .source-text").Text.Trim ();
+					var translation = driver.FindElementByCssSelector (".card .quickdef .lang").Text.Trim ();
 
-					if (!TensesToInclude.Contains(columnNames[column]))
-						continue;
+					if (inputVerb.ExtraInfo != null) {
+						translation = translation + " " + inputVerb.ExtraInfo;
+					}
 
-					for (var row = 0; row < rowNames.Length; row++) {
+					if (term.ToLowerInvariant () != inputVerb.Verb.ToLower ()) {
+						Console.WriteLine ("The verb provided '" + inputVerb.Verb + "' does not match the base form found '" + term + "'.  Please use the base form.");
+						return 1;
+					}
 
-						var selector = "tr:nth-child(" + (row + 2) + ") td:nth-child(" + (column + 2) + ")";
-						var value = indicativeTable.FindElement (By.CssSelector (selector)).Text.Trim().ToLower();
+					foreach (var vtableLabel in driver.FindElementsByCssSelector ("a.vtable-label")) {
+						var vtableType = vtableLabel.Text.Trim ().ToLower ().Replace (" ", "-");
+						driver.ExecuteScript ("arguments[0].classList.add('vtable-label-" + vtableType + "');", vtableLabel);
+					}
+					
+					var infinitiveResult = new Result () {
+						Term = term,
+						TermDefinition = translation
+					};
 
-						var identifier = String.Join (",", new [] {
-							term,
-							columnNames[column],
-							rowNames[row],
+					List<Result> results = new List<Result> ();
+
+					results.Add (infinitiveResult);
+
+					var indicativeTable = driver.FindElementByCssSelector (".vtable-label-indicative + .vtable-wrapper");
+
+					var columnNames = indicativeTable.FindElements (By.CssSelector ("tr:first-child td")).Select (e => e.Text.ToLower ()).Skip (1).ToArray ();
+					var rowNames = indicativeTable.FindElements (By.CssSelector ("tr td:first-child")).Select (e => e.Text.ToLower ()).Skip (1).ToArray ();
+
+					for (var column = 0; column < columnNames.Length; column++) {
+
+						if (!TensesToInclude.Contains (columnNames [column]))
+							continue;
+
+						for (var row = 0; row < rowNames.Length; row++) {
+
+							var selector = "tr:nth-child(" + (row + 2) + ") td:nth-child(" + (column + 2) + ")";
+							var value = indicativeTable.FindElement (By.CssSelector (selector)).Text.Trim ().ToLower ();
+
+							var identifier = String.Join (",", new [] {
+								term,
+								columnNames [column],
+								rowNames [row],
 							});
 
-						var nounPhrase = rowNames [row].Split ('/').First ();
+							var nounPhrase = rowNames [row].Split ('/').First ();
 
-						if (NounsToSkip.Contains (nounPhrase)) {
-							continue;
+							if (NounsToSkip.Contains (nounPhrase)) {
+								continue;
+							}
+
+							results.Add (new Result () {
+								TermConjugationIdentifier = identifier,
+								InfinitiveForm = term,
+								InfinitiveDefinition = translation,
+								Term = value,
+								DeambiguatingNounphrase = nounPhrase
+							});
 						}
+								
+						foreach (var result in results) {
 
-						results.Add (new Result () {
-							TermConjugationIdentifier = identifier,
-							InfinitiveForm = term,
-							InfinitiveDefinition = translation,
-							Term = value,
-							DeambiguatingNounphrase = nounPhrase
-						});
-					}
-							
-					foreach (var result in results) {
+							var termToSearch = result.DeambiguatingNounphrase + " " + result.Term;
 
-						var termToSearch = result.DeambiguatingNounphrase + " " + result.Term;
+							var translateUrl = "http://www.spanishdict.com/translate/" + Uri.EscapeDataString (termToSearch);
+							driver.Navigate ().GoToUrl (translateUrl);
 
-						var translateUrl = "http://www.spanishdict.com/translate/" + Uri.EscapeDataString (termToSearch);
-						driver.Navigate ().GoToUrl (translateUrl);
+							if (result.TermDefinition == null) {
+								string definition = null;
+								do {
+									var selector = ".mt-info.promt .mt-info-text, .quickdef .el";
+									if (driver.FindElementsByCssSelector ("#translate-en").Any ()) {
+										selector = ".mt-info.promt .mt-info-text, #translate-en .quickdef .el";
+									}
 
-						if (result.TermDefinition == null) {
-							string definition = null;
-							do {
-								var selector = ".mt-info.promt .mt-info-text, .quickdef .el";
-								if (driver.FindElementsByCssSelector("#translate-en").Any()) {
-									selector = ".mt-info.promt .mt-info-text, #translate-en .quickdef .el";
-								}
+									definition = driver.FindElementByCssSelector (selector).Text.Trim ().ToLowerInvariant ();
+								} while(definition.Length == 0);
 
-								definition = driver.FindElementByCssSelector (selector).Text.Trim ().ToLowerInvariant ();
-							} while(definition.Length == 0);
-
-							result.TermDefinition = definition;
-						}						
-					}
-				}
-
-				using (var csvWriter = new CsvHelper.CsvWriter(Console.Out))
-				foreach (var result in results) {
-						csvWriter.WriteField (result.Term);
-						csvWriter.WriteField (result.TermDefinition);
-						if (result.InfinitiveForm != null) {
-							csvWriter.WriteField (result.InfinitiveForm + " - " + result.InfinitiveDefinition);
-						} else {
-							csvWriter.WriteField ("");
+								result.TermDefinition = definition;
+							}						
 						}
-						csvWriter.NextRecord ();
+					}
+
+					using (var csvWriter = new CsvHelper.CsvWriter (Console.Out))
+						foreach (var result in results) {
+							csvWriter.WriteField (result.Term);
+							csvWriter.WriteField (result.TermDefinition);
+							if (result.InfinitiveForm != null) {
+								csvWriter.WriteField (result.InfinitiveForm + " - " + result.InfinitiveDefinition);
+							} else {
+								csvWriter.WriteField ("");
+							}
+							csvWriter.NextRecord ();
+						}
 				}
 			}
 
