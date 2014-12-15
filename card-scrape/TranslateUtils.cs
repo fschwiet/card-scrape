84cdbb0d6f7508196a189e4fee274847a946ab92
fschwiet <fschwiet@gmail.com>
2014-12-15 14:40:20 -0600

    when we pick up search results from multiple engines, take the consensus with PROMT breaking any ties

--- card-scrape/TranslateUtils.cs
+++ card-scrape/TranslateUtils.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using OpenQA.Selenium.Remote;
+using System.Collections.Generic;
 using System.Linq;
+
 
 namespace cardscrape
 {
@@ -17,30 +19,58 @@ namespace cardscrape
 			string definition = null;
 
 			do {
-				var selector = ".mt-info.promt .mt-info-text, .quickdef .el";
-
 				//  Checking for an element that doesn't exist requires the fill timeout,
 				//  so we're going to do some timeout switching.
 
 				//  First we do a long search to be sure the page has had time to load whatever
 				//  element we might be looking for, this search should still be fast as typically
 				//  we find something.
-				driver.FindElementByCssSelector(selector);
+				driver.FindElementsByCssSelector(".mt-info.promt .mt-info-text, .quickdef .el");  // the selector is anything we can use
 
 				//  Now we use the shorter timeout for the case where elements.Any() is typically
 				//  false (as WebDriver will wait for the full time)
 				driver.Manage().Timeouts().ImplicitlyWait(ShortWait);
 
-				if (driver.FindElementsByCssSelector ("#translate-en").Any ()) {
-					selector = ".mt-info.promt .mt-info-text, #translate-en .quickdef .el";
+				var externalEngineResults = driver.FindElementsByCssSelector(".mt-info .mt-info-text");
+				if (externalEngineResults.Any()) {
+
+					Dictionary<string,double> scores = new Dictionary<string, double>();
+
+					foreach(var result in externalEngineResults.Select(e => NormalizeString(e.Text))) {
+
+						if (!scores.ContainsKey(result)) {
+							scores[result] = 0;
+						}
+
+						scores[result] += 1;
+					}
+
+					//  Give a slight preference to the PROMT result
+					foreach(var element in driver.FindElementsByCssSelector(".mt-info.promt .mt-info-text")) {
+
+						scores[NormalizeString(element.Text)] += 0.1;
+					}
+
+					definition = scores.OrderByDescending(kvp => kvp.Value).Select(kvp => kvp.Key).First();
+				}
+				else {
+					var selector = ".quickdef .el";
+					if (driver.FindElementsByCssSelector ("#translate-en").Any ()) {
+						selector = " #translate-en .quickdef .el";
+					}
+
+					definition = driver.FindElementByCssSelector (selector).Text.Trim ().ToLowerInvariant ();
+
+					driver.Manage().Timeouts().ImplicitlyWait(LongWait);
 				}
 
-				definition = driver.FindElementByCssSelector (selector).Text.Trim ().ToLowerInvariant ();
-
-				driver.Manage().Timeouts().ImplicitlyWait(LongWait);
 			} while(definition.Length == 0);
 
 			return definition;
+		}
+
+		private static string NormalizeString(string input) {
+			return input.Trim ().ToLowerInvariant ();
 		}
 	}
 }
